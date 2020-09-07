20664e426ddf94212ca2cf0091cdf3f3e0756453
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-09-07 14:55:40 -0500

    can add posts again, but editing is still funky

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -4,6 +4,8 @@ async function newFormHandler(event) {
     const title = document.querySelector('input[name="post-title"]').value;
     const post_text = document.querySelector('textarea[name="post-text"]').value;
 
+    console.log(title, post_text);
+
     const response = await fetch(`/api/posts`, {
         method: 'POST',
         body: JSON.stringify({
@@ -22,4 +24,4 @@ async function newFormHandler(event) {
     }
 };
 
-document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+document.querySelector('.new-post-form').addEventListener('click', newFormHandler);
